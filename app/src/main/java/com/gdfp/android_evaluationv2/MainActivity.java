8e71285e3c5a078b1a65a63ba53109b8a24605fe
danielrpg <daniel.fernandezgp@gmail.com>
2019-09-17 23:28:41 -0400

    First changes

---
+++ app/src/main/java/com/gdfp/android_evaluationv2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.gdfp.android_evaluationv2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
